11122c438822dbce28221375a32f791b24364331
Alexandre Marciano <Alexandre@macbooks-MacBook-Pro.local>
2021-04-07 21:46:39 -0300

    Adicionado: Aula Scrapy com exercício pratico

--- Modulo 2/Coleta de Dados/Scrapy/noticia_coleta/noticia_coleta/spiders/coletanoticia.py
+++ Modulo 2/Coleta de Dados/Scrapy/noticia_coleta/noticia_coleta/spiders/coletanoticia.py
@@ -11,23 +11,23 @@ class Noticia(object):
             return texto.replace("\n","").strip()
 
 
-class NoticiaFolhaSpider(scrapy.Spider):
-    name = 'noticia_folha'
+class ColetanoticiaSpider(scrapy.Spider):
+    name = 'coletanoticia'
     allowed_domains = ['folha.uol.com.br']
     start_urls = ['https://www1.folha.uol.com.br/ultimas-noticias/']
     noticias = []
     def parse(self, response):
-        for noticia_site in response.xpath('//ol[@class="u-list-unstyled"]/li'):         
-            nome = noticia_site.css(".c-headline__title::text").get() # titulo da noticia
-            resumo = noticia_site.css(".c-headline__standfirst::text").get() # resumo da noticia
-            data = noticia_site.css(".c-headline__dateline::text").get() #data da noticia
+        for noticia_site in response.xpath('//ol[@class="u-list-unstyled"]/li'):
+            nome = noticia_site.css(".c-headline__title::text").get() #Captura o Titulo da Noticia
+            resumo = noticia_site.css(".c-headline__standfirst::text").get() #Captura do Resumo da Noticia
+            data = noticia_site.css(".c-headline__dateline::text").get() #Captura da Data da Noticia
             noticia = Noticia(nome,resumo,data)
             print(noticia.__dict__)
             self.add(noticia)
-        with open("nova_noticia.json", "w",encoding='utf8') as file:
+        with open("noticias.json", "w",encoding='utf8') as file:
             json.dump([ob.__dict__ for ob in self.noticias],file,ensure_ascii=False)
             print(file)
-            
+
     def add(self,noticia):
         if (noticia.nome is None or noticia.resumo is None or noticia.data is None):
             return
